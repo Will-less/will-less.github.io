6f25024b7f4729d92fd8185b33bf9b54f5dd1ef7
Willis Yu <willisyu754@gmail.com>
2026-03-05 14:22:02 -0500

    added some js to properly size the images based on the number of columns

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const projects = document.querySelector("project-grid");
+const projects = document.getElementById('project-grid');
+const textBody = document.querySelector('.main-text-box');
+bWidth = textBody.clientWidth;
 
 if(projects) {
-	const projectImages = projects.querySelectorAll("project-image");
+	colNum = getComputedStyle(projects).
+			getPropertyValue("grid-template-columns")?.split(' ').length;
+	const projectImages = projects.querySelectorAll('.project-image');
 
 	projectImages?.forEach((image) => {
-		console.log("test");
+		image.style.width = `${bWidth/colNum}px`;
 	});
 }
-console.log(42);
